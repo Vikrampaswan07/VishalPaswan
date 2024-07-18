984f4b7226eb525258c1ab38c0d2696f2dbde5ed
Vikrampaswan07 <130029534+Vikrampaswan07@users.noreply.github.com>
2024-07-18 14:06:32 +0530

    new

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,7 @@
 import { HERO_CONTENT } from "../constants";
 import './Hero.css';
 import profilePic from "../assets/VishalPaswan.jpg";
+import { motion } from "framer-motion";
 
 const Hero = () => {
   return (
@@ -8,11 +9,20 @@ const Hero = () => {
         <div className="flex flex-wrap">
             <div className="w-full lg:w-1/2">
                 <div className="flex flex-col items-center lg:items-start">
-                    <h1 className="heading">
+                    <motion.h1 
+                    animate={{ x: 100 }}
+                    className="pb-16 text-6xl font-thin tracking-tight lg:mt-16 lg:text-8xl heading">
                         Vishal Paswan
-                    </h1>
-                    <span className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-4xl tracking-tight text-transparent work">Contemporary Choreographer</span>
-                    <p className="my-2 max-w-xl py-6 font-light tracking-tighter description">
+                    </motion.h1>
+                    <motion.span
+                        whileHover={{ scale: 1.2 }}
+                        whileTap={{ scale: 1.1 }}
+                        drag="x"
+                        dragConstraints={{ left: -100, right: 100 }} 
+                        className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-3xl tracking-tight text-transparent work">
+                        Contemporary Choreographer
+                    </motion.span>
+                    <p className="my-3 max-w-xl py-6 font-light tracking-tighter mb-40">
                         {HERO_CONTENT}
                     </p>
                 </div>
